15357004828a8beaba5c071508f0eeec7dbd3baa
akashtyagi <akashdktyagi@gmail.com>
2021-03-29 18:03:52 +0800

    changes in code

--- src/main/java/com/vit/_1_MyFirstCalcProgram/Calculator.java
+++ src/main/java/com/vit/_1_MyFirstCalcProgram/Calculator.java
@@ -5,6 +5,10 @@ public class Calculator {
         return i + i1;
     }
 
+    public int add(int i, int i1, int i2) {
+        return i + i1 + i2;
+    }
+
     public int sub(int i, int i1) {
         return i-i1;
     }
